f115c713832f1d16d1df18c8047f442a3d082507
Tim <ti.hi2.online@gmail.com>
2022-03-03 09:17:41 +0100

    Add: load dataset

--- tf-idf.py
+++ tf-idf.py
@@ -1,1 +1,9 @@
-print("hello world!")
+import numpy as np
+import pandas as pd
+import nltk
+import matplotlib.pyplot as plt
+import os
+print(os.listdir('input'))
+data=pd.read_csv('input/IMDB Dataset.csv')
+print(data.shape)
+print(data.head(10))
