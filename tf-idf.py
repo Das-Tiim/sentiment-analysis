1049871c9e5387efc1c6e8c7ce46ac812a6b5e8e
Tim <ti.hi2.online@gmail.com>
2022-03-03 10:01:53 +0100

    Add: clean and vectorize reviews

--- tf-idf.py
+++ tf-idf.py
@@ -1,9 +1,72 @@
 import numpy as np
 import pandas as pd
-import nltk
 import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.stem.porter import PorterStemmer
+from bs4 import BeautifulSoup
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+
 import os
 print(os.listdir('input'))
 data=pd.read_csv('input/IMDB Dataset.csv')
 print(data.shape)
 print(data.head(10))
+
+tokenizer=ToktokTokenizer()
+
+#Removing the html strips
+def strip_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
+
+#Removing the square brackets
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', '', text)
+
+#Removing the noisy text
+def denoise_text(text):
+    text = strip_html(text)
+    text = remove_between_square_brackets(text)
+    return text
+#Apply function on review column
+data['review']=data['review'].apply(denoise_text)
+
+#Define function for removing special characters
+def remove_special_characters(text, remove_digits=True):
+    pattern=r'[^a-zA-z0-9\s]'
+    text=re.sub(pattern,'',text)
+    return text
+#Apply function on review column
+data['review']=data['review'].apply(remove_special_characters)
+
+#Stemming the text
+def simple_stemmer(text):
+    ps=nltk.porter.PorterStemmer()
+    text= ' '.join([ps.stem(word) for word in text.split()])
+    return text
+#Apply function on review column
+data['review']=data['review'].apply(simple_stemmer)
+
+#removing the stopwords
+stopword_list=nltk.corpus.stopwords.words('english')
+
+def remove_stopwords(text, is_lower_case=False):
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    if is_lower_case:
+        filtered_tokens = [token for token in tokens if token not in stopword_list]
+    else:
+        filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)    
+    return filtered_text
+#Apply function on review column
+data['review']=data['review'].apply(remove_stopwords)
+
+#Tfidf vectorizer
+tv=TfidfVectorizer(min_df=0,max_df=1,use_idf=True,ngram_range=(1,3))
+#transformed reviews
+tv_reviews=tv.fit_transform(data['review'])
+print('Tfidf_train:',tv_reviews.shape)
